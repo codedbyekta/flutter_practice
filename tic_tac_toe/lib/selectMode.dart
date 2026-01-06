ef7f91e9e7acce555a5b4552355ab54fa40518ff
codedbyekta <ekta042704@gmail.com>
2026-01-06 21:22:13 +0530

    Enhance UI with new app bar color and gradient
    Updated the app bar color and added a gradient background to the body.

--- tic_tac_toe/lib/selectMode.dart
+++ tic_tac_toe/lib/selectMode.dart
@@ -10,15 +10,27 @@ class Selectmode extends StatefulWidget {
 class _SelectmodeState extends State<Selectmode> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(  appBar: AppBar(
+    return Scaffold(
+      appBar: AppBar(
         title: Text(
           "TIC TAC TOE",
           style: TextStyle(
+            color: Colors.black,
             fontFamily: "DancingScript",
             fontWeight: FontWeight.bold,
           ),
         ),
-        backgroundColor: Colors.black,
-      ),);
+        backgroundColor: const Color.fromARGB(255, 26, 220, 145),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [const Color.fromARGB(255, 26, 220, 145), const Color.fromARGB(254, 88, 241, 6)],
+            begin: Alignment.bottomRight,
+            end: Alignment.topLeft,
+          ),
+        ),
+      ),
+    );
   }
 }
