8877ec83af38f6a1c0041ba2af3064224fff3cbb
codedbyekta <ekta042704@gmail.com>
2026-01-19 19:20:44 +0530

    Implement HomeScreen layout with gradient background
    Added a Scaffold with a gradient background to the HomeScreen.

--- quiz_app/lib/screens/home_screen.dart
+++ quiz_app/lib/screens/home_screen.dart
@@ -10,6 +10,22 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Colors.white,
+              const Color.fromARGB(255, 196, 244, 244),
+              const Color.fromARGB(255, 196, 244, 244),
+              const Color.fromARGB(255, 196, 244, 244),
+              const Color.fromARGB(255, 196, 244, 244),
+            ],
+            begin: Alignment.bottomRight,
+            end: Alignment.topLeft,
+          ),
+        ),
+      ),
+    );
   }
-} 
+}
