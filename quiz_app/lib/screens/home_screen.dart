930f12df80ad742f7ff62875b891e7db7bdc7355
codedbyekta <ekta042704@gmail.com>
2026-01-20 18:16:39 +0530

    Enhance HomeScreen with app bar and buttons
    Added app bar with category selection buttons for Logical and Quantitative Aptitude.

--- quiz_app/lib/screens/home_screen.dart
+++ quiz_app/lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/screens/logical.dart';
+import 'package:quiz_app/screens/quant.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -11,6 +13,14 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        leading: Image.asset("assets/images/quiz.png"),
+        title: Text(
+          "SELECT CATEGORY",
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.black,
+      ),
       body: Container(
         decoration: BoxDecoration(
           gradient: LinearGradient(
@@ -25,7 +35,50 @@ class _HomeScreenState extends State<HomeScreen> {
             end: Alignment.topLeft,
           ),
         ),
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Image.asset("assets/images/logical.png", width: 120),
+
+              const SizedBox(height: 10),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Logical()),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.black,
+                  foregroundColor: Colors.white,
+                ),
+                child: const Text("LOGICAL APTITUDE"),
+              ),
+
+              const SizedBox(height: 40),
+              Image.asset("assets/images/quant.png", width: 120),
+
+              const SizedBox(height: 10),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Quant()),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.black,
+                  foregroundColor: Colors.white,
+                ),
+                child: const Text("QUANTITATIVE APTITUDE"),
+              ),
+              const SizedBox(height: 40),
+            ],
+          ),
+        ),
       ),
     );
   }
 }
+
