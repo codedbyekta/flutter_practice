3e9d235ecf3a2d860fe91a26ed539f50136e87fa
codedbyekta <ekta042704@gmail.com>
2026-01-22 22:21:26 +0530

    Add files via upload

---
+++ quiz_app/lib/data/ques_ans.dart
@@ -0,0 +1,8 @@
+import 'package:quiz_app/model/question.dart';
+
+List quesAns = [
+  Question(" Find the odd one out from the given set of numbers.\n       14, 28, 35, 46, 56, 84",
+   ["46","84","56","35"]),
+  // Question("Find odd one out from the given jumbled words.\nLNUAIMMIU, ELEST, OPERPC, NMKOYE, INCKLE", ""),
+  // Question("", answer),
+];
