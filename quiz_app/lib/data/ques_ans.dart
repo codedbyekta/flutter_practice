9d8580d72d94bf92ce5f0884a0856647cb9f96c7
codedbyekta <ekta042704@gmail.com>
2026-01-24 22:46:27 +0530

    Update ques_ans.dart to include logical and quant questions
    Refactored question lists to use LogicalQuestion and QuantQuestion classes.

--- quiz_app/lib/data/ques_ans.dart
+++ quiz_app/lib/data/ques_ans.dart
@@ -1,8 +1,20 @@
-import 'package:quiz_app/model/question.dart';
+import 'package:quiz_app/model/logical_question.dart';
+import 'package:quiz_app/model/quant_question.dart';
 
-List quesAns = [
-  Question(" Find the odd one out from the given set of numbers.\n       14, 28, 35, 46, 56, 84",
-   ["46","84","56","35"]),
+List logquesAns = [
+  LogicalQuestion(
+    " Find the odd one out from the given set of numbers.\n14, 28, 35, 46, 56, 84",
+    ["46", "84", "56", "35"],
+  ),
   // Question("Find odd one out from the given jumbled words.\nLNUAIMMIU, ELEST, OPERPC, NMKOYE, INCKLE", ""),
   // Question("", answer),
+];
+
+List quantquesAns = [
+  QuantQuestion("Which of the following is a prime number?", [
+    "517",
+    "559",
+    "571",
+    "533",
+  ]),
 ];
