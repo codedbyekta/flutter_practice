75d9ec163e92183742000ae46bc3c9031bcaf85b
codedbyekta <ekta042704@gmail.com>
2026-02-14 19:59:23 +0530

    Refactor question lists to use Question model
    Refactored question lists to use a unified Question model with correct answers.

--- quiz_app/lib/data/ques_ans.dart
+++ quiz_app/lib/data/ques_ans.dart
@@ -1,20 +1,19 @@
-import 'package:quiz_app/model/logical_question.dart';
-import 'package:quiz_app/model/quant_question.dart';
+import 'package:quiz_app/model/question_model.dart';
 
-List logquesAns = [
-  LogicalQuestion(
-    " Find the odd one out from the given set of numbers.\n14, 28, 35, 46, 56, 84",
-    ["46", "84", "56", "35"],
+List<Question> logquesAns = [
+  Question(
+    question:
+    "Find the odd one out from the given set of numbers.\n14, 28, 35, 46, 56, 84",
+    options: ["46", "84", "56", "35"],
+    correctAnswer: "46",
   ),
-  // Question("Find odd one out from the given jumbled words.\nLNUAIMMIU, ELEST, OPERPC, NMKOYE, INCKLE", ""),
-  // Question("", answer),
 ];
 
-List quantquesAns = [
-  QuantQuestion("Which of the following is a prime number?", [
-    "517",
-    "559",
-    "571",
-    "533",
-  ]),
+List<Question> quantquesAns = [
+  Question(
+    question: "Which of the following is a prime number?",
+    options: ["517", "559", "571", "533"],
+    correctAnswer: "571",
+  ),
 ];
+
