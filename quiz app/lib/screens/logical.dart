ef6ca20a68651d86c9e2ee0235e6179e391e22ed
codedbyekta <ekta042704@gmail.com>
2026-02-17 22:24:48 +0530

    Add files via upload

---
+++ quiz app/lib/screens/logical.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:quiz_app/answerbutton.dart';
+import 'package:quiz_app/data/ques_ans.dart';
+import 'package:quiz_app/quiz_notifier.dart';
+import 'package:quiz_app/result.dart';
+
+class Logical extends StatelessWidget {
+  const Logical({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final quiz = context.watch<QuizNotifier>();
+
+    if (quiz.currentQuesIndex >= logquesAns.length) {
+      return const Scaffold(body: Center(child: CircularProgressIndicator()));
+    }
+
+    final question = logquesAns[quiz.currentQuesIndex];
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "LOGICAL APTITUDE",
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.black,
+        iconTheme: const IconThemeData(color: Colors.white),
+      ),
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.white, Color.fromARGB(255, 196, 244, 244)],
+            begin: Alignment.bottomRight,
+            end: Alignment.topLeft,
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                question.question,
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 20),
+
+              ...question.options.map((ans) {
+                return Answerbutton(
+                  answer: ans,
+                  onTapAnswer: () {
+                    quiz.selectAnswer(question, ans);
+
+                    if (quiz.currentQuesIndex == logquesAns.length) {
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                          builder: (_) =>
+                              ResultScreen(totalQuestions: logquesAns.length),
+                        ),
+                      );
+                    }
+                  },
+                );
+              }).toList(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
